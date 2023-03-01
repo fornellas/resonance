bb6a26517e5c2ff6c70138ab7da944d55b03dc46
Fabio Pugliese Ornellas <fabio.ornellas@gmail.com>
2023-03-01 00:07:29 +0000

    APTPackage.Check

--- resource/apt_package.go
+++ resource/apt_package.go
@@ -3,8 +3,11 @@ package resource
 import (
 	"context"
 	"fmt"
+	"regexp"
+	"strings"
 
 	"github.com/fornellas/resonance/host"
+	"github.com/fornellas/resonance/log"
 )
 
 // APTPackageParams for APTPackage
@@ -26,56 +29,43 @@ func (ap APTPackage) MergeApply() bool {
 	return true
 }
 
-// func (ap APTPackage) GetDesiredState(parameters yaml.Node) (State, error) {
-// 	aptPackageParams := APTPackageParams{}
-// 	if err := parameters.Decode(&aptPackageParams); err != nil {
-// 		return nil, err
-// 	}
-// 	return APTPackageState{
-// 		Version: aptPackageParams.Version,
-// 		Status:  "install ok installed",
-// 	}, nil
-// }
-
-// var aptPackageRegexpStatus = regexp.MustCompile(`^Status: (.+)$`)
-// var aptPackageRegexpVersion = regexp.MustCompile(`^Version: (.+)$`)
-
-// func (ap APTPackage) GetState(ctx context.Context, hst host.Host, name Name) (State, error) {
-// 	hostCmd := host.Cmd{
-// 		Path: "dpkg",
-// 		Args: []string{"-s", name.String()},
-// 	}
-// 	waitStatus, stdout, stderr, err := hst.Run(ctx, hostCmd)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	if !waitStatus.Success() {
-// 		if waitStatus.Exited && waitStatus.ExitCode == 1 && strings.Contains(stderr, "not installed") {
-// 			return APTPackageState{
-// 				Status: "not installed",
-// 			}, nil
-// 		}
-// 		return nil, fmt.Errorf("failed to call '%s': %s\nstdout:\n%s\nstderr:\n%s", hostCmd.String(), waitStatus.String(), stdout, stderr)
-// 	}
-// 	aptPackageState := APTPackageState{}
-// 	for _, line := range strings.Split(stdout, "\n") {
-// 		matches := aptPackageRegexpStatus.FindStringSubmatch(line)
-// 		if len(matches) == 2 {
-// 			aptPackageState.Status = matches[1]
-// 		}
-// 		matches = aptPackageRegexpVersion.FindStringSubmatch(line)
-// 		if len(matches) == 2 {
-// 			aptPackageState.Version = matches[1]
-// 		}
-// 	}
-// 	if aptPackageState.Status == "" || aptPackageState.Version == "" {
-// 		return nil, fmt.Errorf("failed to parse state from '%s': %s\nstdout:\n%s\nstderr:\n%s", hostCmd.String(), waitStatus.String(), stdout, stderr)
-// 	}
-// 	return aptPackageState, nil
-// }
+var aptPackageRegexpStatus = regexp.MustCompile(`^Status: (.+)$`)
+var aptPackageRegexpVersion = regexp.MustCompile(`^Version: (.+)$`)
 
 func (ap APTPackage) Check(ctx context.Context, hst host.Host, instance Instance) (bool, error) {
-	return false, fmt.Errorf("TODO APTPackage.Check")
+	logger := log.GetLogger(ctx)
+
+	hostCmd := host.Cmd{
+		Path: "dpkg",
+		Args: []string{"-s", instance.Name.String()},
+	}
+	waitStatus, stdout, stderr, err := hst.Run(ctx, hostCmd)
+	if err != nil {
+		return false, err
+	}
+	if !waitStatus.Success() {
+		if waitStatus.Exited && waitStatus.ExitCode == 1 && strings.Contains(stderr, "not installed") {
+			logger.Debugf("Not installed")
+			return false, nil
+		}
+		return false, fmt.Errorf("failed to call '%s': %s\nstdout:\n%s\nstderr:\n%s", hostCmd.String(), waitStatus.String(), stdout, stderr)
+	}
+	aptPackageState := APTPackageState{}
+	for _, line := range strings.Split(stdout, "\n") {
+		matches := aptPackageRegexpStatus.FindStringSubmatch(line)
+		if len(matches) == 2 {
+			aptPackageState.Status = matches[1]
+		}
+		matches = aptPackageRegexpVersion.FindStringSubmatch(line)
+		if len(matches) == 2 {
+			aptPackageState.Version = matches[1]
+		}
+	}
+	if aptPackageState.Status == "" || aptPackageState.Version == "" {
+		return false, fmt.Errorf("failed to parse state from '%s': %s\nstdout:\n%s\nstderr:\n%s", hostCmd.String(), waitStatus.String(), stdout, stderr)
+	}
+
+	return true, nil
 }
 
 func (ap APTPackage) Apply(ctx context.Context, hst host.Host, instances []Instance) error {
