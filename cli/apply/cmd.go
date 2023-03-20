b57953dded95e87fadc5cbcf31b21d853530c9f6
Fabio Pugliese Ornellas <fabio.ornellas@gmail.com>
2023-03-20 18:57:40 +0000

    extract plan

--- cli/apply/cmd.go
+++ cli/apply/cmd.go
@@ -1,46 +1,13 @@
 package apply
 
 import (
-	"context"
-
 	"github.com/spf13/cobra"
 
 	"github.com/fornellas/resonance/cli/lib"
-	"github.com/fornellas/resonance/host"
 	"github.com/fornellas/resonance/log"
 	"github.com/fornellas/resonance/resource"
 	"github.com/fornellas/resonance/state"
 )
-
-func readState(
-	ctx context.Context,
-	hst host.Host,
-	newBundle resource.Bundle,
-	hostState *resource.HostState,
-) resource.TypeNameStateMap {
-	logger := log.GetLogger(ctx)
-	typeNames := newBundle.TypeNames()
-	if hostState != nil {
-		for _, typeName := range hostState.PreviousBundle.TypeNames() {
-			duplicate := false
-			for _, tn := range typeNames {
-				if typeName == tn {
-					duplicate = true
-					break
-				}
-			}
-			if duplicate {
-				continue
-			}
-			typeNames = append(typeNames, typeName)
-		}
-	}
-	typeNameStateMap, err := resource.GetTypeNameStateMap(ctx, hst, typeNames)
-	if err != nil {
-		logger.Fatal(err)
-	}
-	return typeNameStateMap
-}
 
 var Cmd = &cobra.Command{
 	Use:   "apply [flags] resources_root",
@@ -49,10 +16,9 @@ var Cmd = &cobra.Command{
 	Args:  cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
 		ctx := cmd.Context()
+		logger := log.GetLogger(ctx)
 
 		root := args[0]
-
-		logger := log.GetLogger(ctx)
 
 		// Host
 		hst, err := lib.GetHost()
@@ -66,59 +32,13 @@ var Cmd = &cobra.Command{
 			logger.Fatal(err)
 		}
 
-		// Load resources
-		newBundle, err := resource.LoadBundle(ctx, hst, root)
-		if err != nil {
-			logger.Fatal(err)
-		}
-
-		// Load saved HostState
-		hostState, err := state.LoadHostState(ctx, persistantState)
-		if err != nil {
-			logger.Fatal(err)
-		}
-		var previousBundle *resource.Bundle
-		if hostState != nil {
-			previousBundle = &hostState.PreviousBundle
-		}
-
-		// Read state
-		typeNameStateMap := readState(ctx, hst, newBundle, hostState)
-
-		// Check saved HostState
-		if hostState != nil {
-			isClean, err := hostState.PreviousBundle.IsClean(ctx, hst, typeNameStateMap)
-			if err != nil {
-				logger.Fatal(err)
-			}
-			if !isClean {
-				logger.Fatalf(
-					"Host state is not clean: this often means external agents altered the host state after previous apply. Try the 'refresh' or 'restore' commands.",
-				)
-			}
-		}
-
-		// Rollback NewRollbackBundle
-		rollbackBundle := resource.NewRollbackBundle(
-			newBundle, previousBundle, typeNameStateMap, resource.ActionConfigure,
-		)
+		// Plan
+		newBundle, plan, rollbackBundle := lib.Plan(ctx, hst, persistantState, root)
 
 		// TODO save rollback bundle
 
-		// Plan
-		plan, err := resource.NewPlan(
-			ctx, hst, newBundle, previousBundle, typeNameStateMap, resource.ActionConfigure,
-		)
-		if err != nil {
-			logger.Fatal(err)
-		}
-		plan.Print(ctx, hst)
-
-		// Execute plan
-		err = plan.Execute(ctx, hst)
-
-		if err == nil {
-			// Save plan state
+		// Execute
+		if err = plan.Execute(ctx, hst); err == nil {
 			newHostState := resource.NewHostState(newBundle)
 			if err := state.SaveHostState(ctx, newHostState, persistantState); err != nil {
 				logger.Fatal(err)
--- cli/lib/lib.go
+++ cli/lib/lib.go
@@ -89,3 +89,89 @@ func Rollback(ctx context.Context, hst host.Host, rollbackBundle resource.Bundle
 
 	logger.Fatal("Failed to apply, rollback to previously saved state successful.")
 }
+
+func readState(
+	ctx context.Context,
+	hst host.Host,
+	newBundle resource.Bundle,
+	hostState *resource.HostState,
+) resource.TypeNameStateMap {
+	logger := log.GetLogger(ctx)
+	typeNames := newBundle.TypeNames()
+	if hostState != nil {
+		for _, typeName := range hostState.PreviousBundle.TypeNames() {
+			duplicate := false
+			for _, tn := range typeNames {
+				if typeName == tn {
+					duplicate = true
+					break
+				}
+			}
+			if duplicate {
+				continue
+			}
+			typeNames = append(typeNames, typeName)
+		}
+	}
+	typeNameStateMap, err := resource.GetTypeNameStateMap(ctx, hst, typeNames)
+	if err != nil {
+		logger.Fatal(err)
+	}
+	return typeNameStateMap
+}
+
+func Plan(
+	ctx context.Context, hst host.Host, persistantState state.PersistantState, root string,
+) (
+	resource.Bundle, resource.Plan, resource.Bundle,
+) {
+	logger := log.GetLogger(ctx)
+
+	// Load resources
+	newBundle, err := resource.LoadBundle(ctx, hst, root)
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	// Load saved HostState
+	hostState, err := state.LoadHostState(ctx, persistantState)
+	if err != nil {
+		logger.Fatal(err)
+	}
+	var previousBundle *resource.Bundle
+	if hostState != nil {
+		previousBundle = &hostState.PreviousBundle
+	}
+
+	// Read state
+	typeNameStateMap := readState(ctx, hst, newBundle, hostState)
+
+	// Check saved HostState
+	if hostState != nil {
+		isClean, err := hostState.PreviousBundle.IsClean(ctx, hst, typeNameStateMap)
+		if err != nil {
+			logger.Fatal(err)
+		}
+		if !isClean {
+			logger.Fatalf(
+				"Host state is not clean: this often means external agents altered the host state after previous apply. Try the 'refresh' or 'restore' commands.",
+			)
+		}
+	}
+
+	// Rollback Bundle
+	rollbackBundle := resource.NewRollbackBundle(
+		newBundle, previousBundle, typeNameStateMap, resource.ActionConfigure,
+	)
+
+	// Plan
+	plan, err := resource.NewPlan(
+		ctx, hst, newBundle, previousBundle, typeNameStateMap, resource.ActionConfigure,
+	)
+	if err != nil {
+		logger.Fatal(err)
+	}
+	plan.Print(ctx, hst)
+
+	return newBundle, plan, rollbackBundle
+}
