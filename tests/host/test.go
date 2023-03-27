dcc589efcff81697fa080f5f446e5640eeb6ff72
Fabio Pugliese Ornellas <fabio.ornellas@gmail.com>
2023-03-27 23:43:02 +0100

    improve mock errors

--- tests/host/test.go
+++ tests/host/test.go
@@ -88,7 +88,6 @@ type Test struct {
 
 func (t *Test) getFuncCall() *FuncCall {
 	if len(t.ExpectedFuncCalls) == 0 {
-		t.T.Fail() // FIXME use T.Fatalf()
 		return nil
 	}
 	testFuncCall, expectedFuncCalls := t.ExpectedFuncCalls[0], t.ExpectedFuncCalls[1:]
@@ -101,11 +100,10 @@ func (t Test) Chmod(ctx context.Context, name string, mode os.FileMode) error {
 	logger.Debugf("Chmod %v %s", mode, name)
 	funcCall := t.getFuncCall()
 	if funcCall == nil {
-		return fmt.Errorf("no more calls expected: Chmod(%v, %v)", name, mode)
+		t.T.Fatalf("no more calls expected: Chmod(%v, %v)", name, mode)
 	}
 	if funcCall.Chmod == nil {
-		t.T.Fail() // FIXME use T.Fatalf()
-		return fmt.Errorf("unexpected call: got Chmod(%v, %v), expected %#v", name, mode, funcCall)
+		t.T.Fatalf("unexpected call: got Chmod(%v, %v), expected %#v", name, mode, funcCall)
 	}
 	return funcCall.Chmod.ReturnError
 }
@@ -115,11 +113,10 @@ func (t Test) Chown(ctx context.Context, name string, uid, gid int) error {
 	logger.Debugf("Chown %v %v %s", uid, gid, name)
 	funcCall := t.getFuncCall()
 	if funcCall == nil {
-		return fmt.Errorf("no more calls expected: Chown(%v, %v, %v)", name, uid, gid)
+		t.T.Fatalf("no more calls expected: Chown(%v, %v, %v)", name, uid, gid)
 	}
 	if funcCall.Chown == nil {
-		t.T.Fail() // FIXME use T.Fatalf()
-		return fmt.Errorf("unexpected call: got Chown(%v, %v, %v), expected %#v", name, uid, gid, funcCall)
+		t.T.Fatalf("unexpected call: got Chown(%v, %v, %v), expected %#v", name, uid, gid, funcCall)
 	}
 	return funcCall.Chown.ReturnError
 }
@@ -132,8 +129,7 @@ func (t Test) Lookup(ctx context.Context, username string) (*user.User, error) {
 		return nil, fmt.Errorf("no more calls expected: got Lookup(%v)", username)
 	}
 	if funcCall.Lookup == nil {
-		t.T.Fail() // FIXME use T.Fatalf()
-		return nil, fmt.Errorf("unexpected call: got Lookup(%v), expected %#v", username, funcCall)
+		t.T.Fatalf("unexpected call: got Lookup(%v), expected %#v", username, funcCall)
 	}
 	return funcCall.Lookup.ReturnUser, funcCall.Lookup.ReturnError
 }
@@ -146,8 +142,7 @@ func (t Test) LookupGroup(ctx context.Context, name string) (*user.Group, error)
 		return nil, fmt.Errorf("no more calls expected: got LookupGroup(%v)", name)
 	}
 	if funcCall.LookupGroup == nil {
-		t.T.Fail() // FIXME use T.Fatalf()
-		return nil, fmt.Errorf("unexpected call: got LookupGroup(%v), expected %#v", name, funcCall)
+		t.T.Fatalf("unexpected call: got LookupGroup(%v), expected %#v", name, funcCall)
 	}
 	return funcCall.LookupGroup.ReturnGroup, funcCall.LookupGroup.ReturnError
 }
@@ -157,11 +152,10 @@ func (t Test) Lstat(ctx context.Context, name string) (os.FileInfo, error) {
 	logger.Debugf("Lstat %s", name)
 	funcCall := t.getFuncCall()
 	if funcCall == nil {
-		return nil, fmt.Errorf("no more calls expected: got Lstat(%v)", name)
+		t.T.Fatalf("no more calls expected: got Lstat(%v)", name)
 	}
 	if funcCall.Lstat == nil {
-		t.T.Fail() // FIXME use T.Fatalf()
-		return nil, fmt.Errorf("unexpected call: got Lstat(%v), expected %#v", name, funcCall)
+		t.T.Fatalf("unexpected call: got Lstat(%v), expected %#v", name, funcCall)
 	}
 	return funcCall.Lstat.ReturnFileInfo, funcCall.Lstat.ReturnError
 }
@@ -171,11 +165,10 @@ func (t Test) ReadFile(ctx context.Context, name string) ([]byte, error) {
 	logger.Debugf("ReadFile %s", name)
 	funcCall := t.getFuncCall()
 	if funcCall == nil {
-		return nil, fmt.Errorf("no more calls expected: got ReadFile(%v)", name)
+		t.T.Fatalf("no more calls expected: got ReadFile(%v)", name)
 	}
 	if funcCall.ReadFile == nil {
-		t.T.Fail() // FIXME use T.Fatalf()
-		return nil, fmt.Errorf("unexpected call: got ReadFile(%v), expected %#v", name, funcCall)
+		t.T.Fatalf("unexpected call: got ReadFile(%v), expected %#v", name, funcCall)
 	}
 	return funcCall.ReadFile.ReturnBytes, funcCall.ReadFile.ReturnError
 }
@@ -185,11 +178,10 @@ func (t Test) Remove(ctx context.Context, name string) error {
 	logger.Debugf("Remove %s", name)
 	funcCall := t.getFuncCall()
 	if funcCall == nil {
-		return fmt.Errorf("no more calls expected: got Remove(%v)", name)
+		t.T.Fatalf("no more calls expected: got Remove(%v)", name)
 	}
 	if funcCall.Remove == nil {
-		t.T.Fail() // FIXME use T.Fatalf()
-		return fmt.Errorf("unexpected call: got Remove(%v), expected %#v", name, funcCall)
+		t.T.Fatalf("unexpected call: got Remove(%v), expected %#v", name, funcCall)
 	}
 	return funcCall.Remove.ReturnError
 }
@@ -199,11 +191,10 @@ func (t Test) Run(ctx context.Context, cmd host.Cmd) (host.WaitStatus, string, s
 	logger.Debugf("Run %s", cmd)
 	funcCall := t.getFuncCall()
 	if funcCall == nil {
-		return host.WaitStatus{}, "", "", fmt.Errorf("no more calls expected: got Run(%v)", cmd)
+		t.T.Fatalf("no more calls expected: got Run(%v)", cmd)
 	}
 	if funcCall.Run == nil {
-		t.T.Fail() // FIXME use T.Fatalf()
-		return host.WaitStatus{}, "", "", fmt.Errorf("unexpected call: got Run(%v), expected %#v", cmd, funcCall)
+		t.T.Fatalf("unexpected call: got Run(%v), expected %#v", cmd, funcCall)
 	}
 	return funcCall.Run.ReturnWaitStatus, funcCall.Run.ReturnStdout, funcCall.Run.ReturnStderr, funcCall.Run.ReturnError
 }
@@ -213,11 +204,10 @@ func (t Test) WriteFile(ctx context.Context, name string, data []byte, perm os.F
 	logger.Debugf("WriteFile %s %v", name, perm)
 	funcCall := t.getFuncCall()
 	if funcCall == nil {
-		return fmt.Errorf("no more calls expected: got WriteFile(%v, %v, %v)", name, data, perm)
+		t.T.Fatalf("no more calls expected: got WriteFile(%v, %v, %v)", name, data, perm)
 	}
 	if funcCall.WriteFile == nil {
-		t.T.Fail() // FIXME use T.Fatalf()
-		return fmt.Errorf("unexpected call: got WriteFile(%v, %v, %v), expected %#v", name, data, perm, funcCall)
+		t.T.Fatalf("unexpected call: got WriteFile(%v, %v, %v), expected %#v", name, data, perm, funcCall)
 	}
 	return funcCall.WriteFile.ReturnError
 }
