37ce9b925e9b2b0a0d200b99f1099e7fdb2bdc7c
Fabio Pugliese Ornellas <fabio.ornellas@gmail.com>
2024-08-24 20:39:05 +0100

    Update store interface
    This PR implements a new interface for storing state, which will be used by subsequent PRs that will implement plan, apply etc commands.
    
    This PR also completely deletes the old broken `plan.go' as it'll be almost entirely rewritten: no point in fixing build errors here.
    
    commit-id:9a999b86

---
+++ internal/store/common_linux_test.go
@@ -0,0 +1,106 @@
+package store
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	blueprintPkg "github.com/fornellas/resonance/internal/blueprint"
+	"github.com/fornellas/resonance/internal/diff"
+	hostPkg "github.com/fornellas/resonance/internal/host"
+	"github.com/fornellas/resonance/log"
+	resourcesPkg "github.com/fornellas/resonance/resources"
+)
+
+func getTestBlueprint(t *testing.T, ctx context.Context) *blueprintPkg.Blueprint {
+	resources := resourcesPkg.Resources{
+		&resourcesPkg.File{
+			Path: "/tmp/foo",
+			Uid:  123,
+			Gid:  456,
+		},
+		&resourcesPkg.APTPackage{
+			Package: "foo",
+			Version: "1.2.3",
+		},
+	}
+	host := hostPkg.Local{}
+	blueprint, err := blueprintPkg.NewBlueprintFromResources(ctx, resources, host)
+	require.NoError(t, err)
+	return &blueprint
+}
+
+func testStore(t *testing.T, store Store) {
+	ctx := context.Background()
+	ctx = log.WithTestLogger(ctx)
+
+	t.Run("OriginalResource", func(t *testing.T) {
+		resources := resourcesPkg.Resources{
+			&resourcesPkg.File{
+				Path: "/tmp/foo",
+				Uid:  123,
+				Gid:  456,
+			},
+			&resourcesPkg.APTPackage{
+				Package: "foo",
+				Version: "1.2.3",
+			},
+		}
+		for _, resource := range resources {
+			hasOriginalResource, err := store.HasOriginalResource(ctx, resource)
+			require.NoError(t, err)
+			require.False(t, hasOriginalResource)
+
+			require.NoError(t, store.SaveOriginalResource(ctx, resource))
+
+			hasOriginalResource, err = store.HasOriginalResource(ctx, resource)
+			require.NoError(t, err)
+			require.True(t, hasOriginalResource)
+
+			loadedResource, err := store.LoadOriginalResource(ctx, resourcesPkg.NewResourceWithSameId(resource))
+			require.NoError(t, err)
+			require.Empty(t, diff.DiffAsYaml(resource, loadedResource))
+		}
+
+	})
+
+	t.Run("LastBlueprint", func(t *testing.T) {
+		nilBlueprint, err := store.LoadLastBlueprint(ctx)
+		require.NoError(t, err)
+		require.Nil(t, nilBlueprint)
+
+		blueprint := getTestBlueprint(t, ctx)
+
+		require.NoError(t, store.SaveLastBlueprint(ctx, blueprint))
+
+		loadedBlueprint, err := store.LoadLastBlueprint(ctx)
+		require.NoError(t, err)
+		require.Empty(t, diff.DiffAsYaml(blueprint, loadedBlueprint))
+	})
+
+	t.Run("LastBlueprint", func(t *testing.T) {
+		hasTargetBlueprint, err := store.HasTargetBlueprint(ctx)
+		require.NoError(t, err)
+		require.False(t, hasTargetBlueprint)
+
+		blueprint := getTestBlueprint(t, ctx)
+
+		require.NoError(t, store.SaveTargetBlueprint(ctx, blueprint))
+		require.NoError(t, err)
+
+		hasTargetBlueprint, err = store.HasTargetBlueprint(ctx)
+		require.NoError(t, err)
+		require.True(t, hasTargetBlueprint)
+
+		loadedBlueprint, err := store.LoadTargetBlueprint(ctx)
+		require.NoError(t, err)
+		require.Empty(t, diff.DiffAsYaml(blueprint, loadedBlueprint))
+
+		require.NoError(t, store.DeleteTargetBlueprint(ctx))
+
+		hasTargetBlueprint, err = store.HasTargetBlueprint(ctx)
+		require.NoError(t, err)
+		require.False(t, hasTargetBlueprint)
+	})
+}
--- internal/store/host.go
+++ internal/store/host.go
@@ -12,31 +12,180 @@ import (
 
 	"github.com/fornellas/resonance/host"
 	blueprintPkg "github.com/fornellas/resonance/internal/blueprint"
-	"github.com/fornellas/resonance/log"
+	resourcesPkg "github.com/fornellas/resonance/resources"
 )
+
+type resourceStoreSchema map[string]map[string]resourcesPkg.Resource
+
+func (s *resourceStoreSchema) UnmarshalYAML(node *yaml.Node) error {
+	type UnmarshalSchema map[string]map[string]yaml.Node
+
+	resourceStore := UnmarshalSchema{}
+
+	node.KnownFields(true)
+	err := node.Decode(&resourceStore)
+	if err != nil {
+		return fmt.Errorf("line %d: %s", node.Line, err.Error())
+	}
+
+	for typeName, idResourceMap := range resourceStore {
+		(*s)[typeName] = map[string]resourcesPkg.Resource{}
+		for id, resourceNode := range idResourceMap {
+			resource := resourcesPkg.GetResourceByTypeName(typeName)
+			if resource == nil {
+				return fmt.Errorf("line %d: invalid single resource type: %#v ", resourceNode.Line, typeName)
+			}
+			resourceNode.KnownFields(true)
+			err := resourceNode.Decode(resource)
+			if err != nil {
+				return fmt.Errorf("line %d: %s", resourceNode.Line, err.Error())
+			}
+			(*s)[typeName][id] = resource
+		}
+	}
+
+	return nil
+}
 
 // Implementation of Store that persists Blueprints at a Host at Path.
 type HostStore struct {
-	Host host.Host
-	Path string
+	Host          host.Host
+	basePath      string
+	resourcesPath string
 }
 
 // NewHostStore creates a new HostStore for given Host.
 func NewHostStore(hst host.Host, path string) *HostStore {
+	// prefix the store path with a DB version, so we can handle changes in the store format
+	storePath := filepath.Join(path, "v1")
 	return &HostStore{
-		Host: hst,
-		Path: path,
+		Host:          hst,
+		basePath:      storePath,
+		resourcesPath: filepath.Join(storePath, "resources"),
 	}
 }
 
-func (s *HostStore) getYamlPath() string {
-	return filepath.Join(s.Path, "blueprint.yaml")
+func (s *HostStore) hasFile(ctx context.Context, path string) (bool, error) {
+	_, err := s.Host.Lstat(ctx, path)
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			return false, nil
+		}
+		return false, err
+	}
+	return true, nil
 }
 
-func (s *HostStore) GetLastBlueprint(ctx context.Context) (blueprintPkg.Blueprint, error) {
-	logger := log.MustLogger(ctx)
-	logger.Info("📂 Loading last stored Bueprint")
-	blueprintBytes, err := s.Host.ReadFile(ctx, s.getYamlPath())
+func (s *HostStore) getResourcePath(resource resourcesPkg.Resource) string {
+	hash := resourcesPkg.HashResource(resource)
+	return fmt.Sprintf("%s.yaml", filepath.Join(s.resourcesPath, hash))
+}
+
+func (s *HostStore) SaveOriginalResource(ctx context.Context, resource resourcesPkg.Resource) error {
+	path := s.getResourcePath(resource)
+
+	hasFile, err := s.hasFile(ctx, path)
+	if err != nil {
+		return err
+	}
+
+	if hasFile {
+		return errors.New("TODO SaveOriginalResource")
+	} else {
+		resourceStore := resourceStoreSchema{
+			resourcesPkg.GetResourceTypeName(resource): map[string]resourcesPkg.Resource{
+				resourcesPkg.GetResourceId(resource): resource,
+			},
+		}
+		return s.saveYaml(ctx, resourceStore, path)
+	}
+}
+
+func (s *HostStore) HasOriginalResource(ctx context.Context, resource resourcesPkg.Resource) (bool, error) {
+	path := s.getResourcePath(resource)
+
+	hasFile, err := s.hasFile(ctx, path)
+	if err != nil {
+		return false, err
+	}
+	if !hasFile {
+		return false, nil
+	}
+
+	resourceStoreBytes, err := s.Host.ReadFile(ctx, path)
+	if err != nil {
+		return false, err
+	}
+	resourceStore := resourceStoreSchema{}
+	err = yaml.Unmarshal(resourceStoreBytes, &resourceStore)
+	if err != nil {
+		return false, err
+	}
+
+	resourceTypeMap, ok := resourceStore[resourcesPkg.GetResourceTypeName(resource)]
+	if !ok {
+		return false, nil
+	}
+
+	_, ok = resourceTypeMap[resourcesPkg.GetResourceId(resource)]
+	return ok, nil
+}
+
+func (s *HostStore) LoadOriginalResource(ctx context.Context, resource resourcesPkg.Resource) (resourcesPkg.Resource, error) {
+	path := s.getResourcePath(resource)
+
+	resourceStoreBytes, err := s.Host.ReadFile(ctx, path)
+	if err != nil {
+		return nil, err
+	}
+
+	resourceStore := resourceStoreSchema{}
+	err = yaml.Unmarshal(resourceStoreBytes, &resourceStore)
+	if err != nil {
+		return nil, err
+	}
+
+	resourceTypeMap, ok := resourceStore[resourcesPkg.GetResourceTypeName(resource)]
+	if !ok {
+		return nil, errors.New("original resource not found")
+	}
+
+	originalResource, ok := resourceTypeMap[resourcesPkg.GetResourceId(resource)]
+	if !ok {
+		return nil, errors.New("original resource not found")
+	}
+
+	return originalResource, nil
+}
+
+func (s *HostStore) getBlueprintPath(name string) string {
+	return filepath.Join(s.basePath, fmt.Sprintf("%s.yaml", name))
+}
+
+func (s *HostStore) saveYaml(ctx context.Context, obj any, path string) error {
+	blueprintBytes, err := yaml.Marshal(obj)
+	if err != nil {
+		panic(fmt.Sprintf("bug: failed to serialize blueprint: %s", err.Error()))
+	}
+
+	dir := filepath.Dir(path)
+
+	if err := os.MkdirAll(dir, fs.FileMode(0700)); err != nil {
+		return err
+	}
+
+	return s.Host.WriteFile(ctx, path, blueprintBytes, os.FileMode(0600))
+}
+
+func (s *HostStore) SaveLastBlueprint(ctx context.Context, blueprint *blueprintPkg.Blueprint) error {
+	path := s.getBlueprintPath("last")
+	return s.saveYaml(ctx, blueprint, path)
+}
+
+func (s *HostStore) loadBlueprint(ctx context.Context, name string) (*blueprintPkg.Blueprint, error) {
+	path := s.getBlueprintPath(name)
+
+	blueprintBytes, err := s.Host.ReadFile(ctx, path)
 	if err != nil {
 		if errors.Is(err, os.ErrNotExist) {
 			return nil, nil
@@ -44,8 +193,8 @@ func (s *HostStore) GetLastBlueprint(ctx context.Context) (blueprintPkg.Blueprin
 		return nil, err
 	}
 
-	blueprint := blueprintPkg.Blueprint{}
-	err = yaml.Unmarshal(blueprintBytes, &blueprint)
+	blueprint := &blueprintPkg.Blueprint{}
+	err = yaml.Unmarshal(blueprintBytes, blueprint)
 	if err != nil {
 		return nil, err
 	}
@@ -53,20 +202,32 @@ func (s *HostStore) GetLastBlueprint(ctx context.Context) (blueprintPkg.Blueprin
 	return blueprint, nil
 }
 
-func (s *HostStore) Save(ctx context.Context, blueprint blueprintPkg.Blueprint) error {
-	logger := log.MustLogger(ctx)
-	logger.Info("💾 Saving Bueprint to host")
+func (s *HostStore) LoadLastBlueprint(ctx context.Context) (*blueprintPkg.Blueprint, error) {
+	return s.loadBlueprint(ctx, "last")
+}
 
-	blueprintBytes, err := yaml.Marshal(blueprint)
+func (s *HostStore) SaveTargetBlueprint(ctx context.Context, blueprint *blueprintPkg.Blueprint) error {
+	path := s.getBlueprintPath("target")
+	return s.saveYaml(ctx, blueprint, path)
+}
+
+func (s *HostStore) HasTargetBlueprint(ctx context.Context) (bool, error) {
+	path := s.getBlueprintPath("target")
+	return s.hasFile(ctx, path)
+}
+
+func (s *HostStore) LoadTargetBlueprint(ctx context.Context) (*blueprintPkg.Blueprint, error) {
+	return s.loadBlueprint(ctx, "target")
+}
+
+func (s *HostStore) DeleteTargetBlueprint(ctx context.Context) error {
+	path := s.getBlueprintPath("target")
+	err := s.Host.Remove(ctx, path)
 	if err != nil {
-		panic(fmt.Sprintf("bug: failed to serialize blueprint: %s", err.Error()))
-	}
-
-	dir := filepath.Dir(s.getYamlPath())
-
-	if err := os.MkdirAll(dir, fs.FileMode(0700)); err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			return nil
+		}
 		return err
 	}
-
-	return s.Host.WriteFile(ctx, s.getYamlPath(), blueprintBytes, os.FileMode(0600))
+	return nil
 }
