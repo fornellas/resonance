6c71c916d9c855bf026e63690382ba554b76e0a6
Fabio Pugliese Ornellas <fabio.ornellas@gmail.com>
2025-04-06 14:57:05 +0100

    fix build

--- log/context.go
+++ log/context.go
@@ -18,16 +18,18 @@ func WithLogger(ctx context.Context, logger *slog.Logger) context.Context {
 
 // Similar to [WithLogger], but constructs a new logger suitable for using during testss.
 func WithTestLogger(ctx context.Context) context.Context {
-	handler := NewConsoleHandler(os.Stderr, ConsoleHandlerOptions{
-		Level:     slog.LevelDebug,
-		AddSource: true,
-		Time:      false,
-		ReplaceAttr: func(groups []string, attr slog.Attr) slog.Attr {
-			if len(groups) == 0 && attr.Key == "version" {
-				return slog.Attr{}
-			}
-			return attr
+	handler := NewConsoleHandler(os.Stderr, &ConsoleHandlerOptions{
+		HandlerOptions: slog.HandlerOptions{
+			Level:     slog.LevelDebug,
+			AddSource: true,
+			ReplaceAttr: func(groups []string, attr slog.Attr) slog.Attr {
+				if len(groups) == 0 && attr.Key == "version" {
+					return slog.Attr{}
+				}
+				return attr
+			},
 		},
+		Time: false,
 	})
 	logger := slog.New(handler)
 	return WithLogger(ctx, logger)
@@ -44,23 +46,8 @@ func MustLogger(ctx context.Context) *slog.Logger {
 	return logger
 }
 
-// MustContextLoggerSection creates a new log section.
-// It fetches a logger value from the context, previously set with WithLogger, and logs given
-// msg and args as info.
-// If its handler implements [SectionHandler], then it WithSection(), and set the new logger value
-// to a copy of the context.
-// If the handler does not implement [SectionHandler], return the original context and the
-// retrieved logger.
-// It panics if no logger value has been set previously.
+// FIXME replace by slog.Handler.WithGroup
 func MustContextLoggerWithSection(ctx context.Context, msg string, args ...any) (context.Context, *slog.Logger) {
-	logger := MustLogger(ctx)
-	logger.Info(msg, args...)
-	if logger.Handler().Enabled(ctx, slog.LevelInfo) {
-		handler, ok := logger.Handler().(SectionHandler)
-		if ok {
-			logger = slog.New(handler.WithSection())
-			ctx = WithLogger(ctx, logger)
-		}
-	}
-	return ctx, logger
+	logger := MustLogger(ctx).WithGroup(msg).With(args...)
+	return WithLogger(ctx, logger), logger
 }
