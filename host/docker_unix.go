7fd20b769a4f20fc2634439ae2c762a397f1cdce
Fabio Pugliese Ornellas <fabio.ornellas@gmail.com>
2025-08-22 01:44:55 +0100

    move & fix GetTestDockerHost

--- host/docker_unix.go
+++ host/docker_unix.go
@@ -1,6 +1,7 @@
 package host
 
 import (
+	"bytes"
 	"context"
 	"errors"
 	"fmt"
@@ -10,8 +11,13 @@ import (
 	"path/filepath"
 	"strings"
 	"syscall"
+	"testing"
+	"time"
 
 	"al.essio.dev/pkg/shellescape"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 
 	"github.com/fornellas/resonance/host/types"
 )
@@ -116,4 +122,96 @@ func (h Docker) Type() string {
 
 func (h Docker) Close(ctx context.Context) error {
 	return nil
+}
+
+// GetTestDockerHost creates a Docker BaseHost suitable for usage in tests. It returns the host and
+// the docker connection string to it. The container will be purged when the test finishes.
+func GetTestDockerHost(t *testing.T, image string) (Docker, string) {
+	if _, err := exec.LookPath("docker"); err != nil {
+		t.Skip("docker command not found on path")
+	}
+
+	ctx, cancel := context.WithCancel(t.Context())
+	sanitizedName := strings.Map(func(r rune) rune {
+		if (r >= 'a' && r <= 'z') || (r >= 'A' && r <= 'Z') || (r >= '0' && r <= '9') || r == '_' || r == '.' || r == '-' {
+			return r
+		}
+		return '_'
+	}, t.Name())
+	name := fmt.Sprintf("resonance-test-%s-%d", sanitizedName, os.Getpid())
+
+	deadline, ok := t.Deadline()
+	timeout := 5 * time.Minute
+	if !ok {
+		timeout = time.Until(deadline)
+	}
+
+	cmd := exec.CommandContext(
+		ctx,
+		"docker", "run",
+		"--name", name,
+		"--rm",
+		image,
+		"sleep", fmt.Sprintf("%d", int(timeout.Seconds())),
+	)
+	stdout := bytes.Buffer{}
+	cmd.Stdout = &stdout
+	stderr := bytes.Buffer{}
+	cmd.Stderr = &stderr
+	cmd.Start()
+	t.Cleanup(func() {
+		cancel()
+		err := cmd.Wait()
+		var exitError *exec.ExitError
+		if errors.As(err, &exitError) {
+			if waitStatus, ok := exitError.Sys().(syscall.WaitStatus); ok {
+				if waitStatus.Signaled() && waitStatus.Signal() == syscall.SIGKILL {
+					return
+				}
+			}
+		}
+		assert.NoError(t, err, fmt.Sprintf(
+			"docker run returned error:\nstdout:\n%s\nstderr\n%s", stdout.String(), stderr.String()),
+		)
+	})
+
+	timeoutCh := time.After(timeout)
+	for {
+		select {
+		case <-timeoutCh:
+			t.Fatalf("timeout waiting for container")
+		case <-time.After(100 * time.Millisecond):
+		}
+		cmdCheck := exec.CommandContext(ctx, "docker", "exec", name, "/bin/true")
+		stdoutBuffer := bytes.Buffer{}
+		cmdCheck.Stdout = &stdoutBuffer
+		stderrBuffer := bytes.Buffer{}
+		cmdCheck.Stderr = &stderrBuffer
+		err := cmdCheck.Run()
+		if err != nil {
+			var exitError *exec.ExitError
+			if ok := errors.As(err, &exitError); ok {
+				if exitError.Exited() {
+					if strings.Contains(stderrBuffer.String(), "No such container") {
+						continue
+					}
+					if strings.Contains(stderrBuffer.String(), "is not running") {
+						continue
+					}
+				}
+			}
+			require.NoErrorf(
+				t, err,
+				"failed: %s %s:\nstdout:\n%s\n\nstderr\n%s", cmd.Path, cmd.Args, stdoutBuffer.String(), stderrBuffer.String(),
+			)
+		}
+		break
+	}
+
+	connection := fmt.Sprintf("0:0@%s", name)
+	dockerHost, err := NewDocker(ctx, connection)
+	require.NoError(t, err)
+	t.Cleanup(func() { require.NoError(t, dockerHost.Close(ctx)) })
+
+	return dockerHost, connection
 }
