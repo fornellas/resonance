c647ba1051ddae5d99c7f942b4277c2402cd5f26
Fabio Pugliese Ornellas <fabio.ornellas@gmail.com>
2025-08-23 19:16:03 +0100

    add tests

--- resources/dpkg_arch_test.go
+++ resources/dpkg_arch_test.go
@@ -96,3 +96,23 @@ func TestDpkgArch(t *testing.T) {
 		}
 	})
 }
+
+func TestDpkgArch_Satisfies(t *testing.T) {
+	t.Run("Satisfies()", func(t *testing.T) {
+		a := &DpkgArch{ForeignArchitectures: []string{"i386", "arm64"}}
+		b := &DpkgArch{ForeignArchitectures: []string{"i386"}}
+		require.True(t, a.Satisfies(b))
+		require.False(t, b.Satisfies(a))
+		require.True(t, a.Satisfies(&DpkgArch{ForeignArchitectures: []string{}}))
+		require.True(t, (&DpkgArch{ForeignArchitectures: []string{}}).Satisfies(&DpkgArch{ForeignArchitectures: []string{}}))
+		require.False(t, (&DpkgArch{ForeignArchitectures: []string{}}).Satisfies(a))
+	})
+}
+
+func TestDpkgArch_Validate(t *testing.T) {
+	t.Run("Validate()", func(t *testing.T) {
+		require.NoError(t, (&DpkgArch{ForeignArchitectures: []string{"i386", "amd64"}}).Validate())
+		require.Error(t, (&DpkgArch{ForeignArchitectures: []string{"!"}}).Validate())
+		require.Error(t, (&DpkgArch{ForeignArchitectures: []string{"amd64", "bad!"}}).Validate())
+	})
+}
