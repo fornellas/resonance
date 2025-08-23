ae949c7260b430b74b2d61c9b9f7a3faa3b33b7a
Fabio Pugliese Ornellas <fabio.ornellas@gmail.com>
2025-08-23 19:16:18 +0100

    rename tests

--- resources/dpkg_arch_test.go
+++ resources/dpkg_arch_test.go
@@ -95,9 +95,6 @@ func TestDpkgArch(t *testing.T) {
 			})
 		}
 	})
-}
-
-func TestDpkgArch_Satisfies(t *testing.T) {
 	t.Run("Satisfies()", func(t *testing.T) {
 		a := &DpkgArch{ForeignArchitectures: []string{"i386", "arm64"}}
 		b := &DpkgArch{ForeignArchitectures: []string{"i386"}}
@@ -107,9 +104,6 @@ func TestDpkgArch_Satisfies(t *testing.T) {
 		require.True(t, (&DpkgArch{ForeignArchitectures: []string{}}).Satisfies(&DpkgArch{ForeignArchitectures: []string{}}))
 		require.False(t, (&DpkgArch{ForeignArchitectures: []string{}}).Satisfies(a))
 	})
-}
-
-func TestDpkgArch_Validate(t *testing.T) {
 	t.Run("Validate()", func(t *testing.T) {
 		require.NoError(t, (&DpkgArch{ForeignArchitectures: []string{"i386", "amd64"}}).Validate())
 		require.Error(t, (&DpkgArch{ForeignArchitectures: []string{"!"}}).Validate())
